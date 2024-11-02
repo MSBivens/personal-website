98bb9eb237277de496d0e264b2cda9631d30a78f
Michael Bivens <mikebiv95@gmail.com>
2024-11-02 15:31:08 -0700

    updating portfolio

--- personal_site/src/app/dev/page.js
+++ personal_site/src/app/dev/page.js
@@ -6,8 +6,12 @@ export default function Dev() {
         You can download a PDF copy of my resume{" "}
         <a href="../../files/mbivens_resume.pdf">here</a>.
       </p>
-      <h2>Select Projects</h2>
-      {/* <h3>TBD</h3>
+      <p>
+        After leaving Web3 behind, I&apos;m working on building a new portfolio
+        of projects. I&apos;ll have more shortly!
+      </p>
+      {/* <h2>Select Projects</h2>
+      <h3>TBD</h3>
       <p>TBD</p>
       <ul className="list-disc" style={{ color: "black", marginLeft: "2em" }}>
         <li style={{ marginBottom: "1rem" }}>
@@ -21,164 +25,38 @@ export default function Dev() {
           </p>
         </li>
       </ul> */}
-      <h3>dNFT Character Builder Chainlink&apos;s Spring 2022 Hackathon</h3>
+      {/* DeltaSigVandals.org */}
+      <h3>Fraternity Website for Alumni</h3>
       <p>
-        This application allows users to mint and customize dynamic NFTs on the
-        Polygon Network, update their metadata via smart contracts, and generate
-        randomized characters using Chainlink&apos;s VRV.
+        This is a marketing website to funnel alumni members toward donating to
+        support efforts like scholarship endowments and facility upgrades.
       </p>
       <ul className="list-disc" style={{ color: "black", marginLeft: "2em" }}>
         <li style={{ marginBottom: "1rem" }}>
           <p>
-            <a href="https://devpost.com/software/dnft-character-builder?ref_content=my-projects-tab&ref_feature=my_projects">
-              DevPost Page
-            </a>
+            <a href="https://www.deltasigvandals.org/">Live Site</a>
           </p>
         </li>
         <li style={{ marginBottom: "1rem" }}>
           <p>
-            <a href="https://github.com/MSBivens/dnft_character_builder">
-              Github Repo
-            </a>
+            <a href="https://github.com/MSBivens/dsp">Github Repo</a>
           </p>
         </li>
       </ul>
-      <h3>Community Funding with ERC1155 for Polygon BUIDL IT 2022</h3>
+      {/* TAAM */}
+      <h3>Theatre Audition Application Manager (TAAM)</h3>
       <p>
-        This project offers an affordable crowdfunding tool where founders can
-        mint and manage campaigns, supporters can buy tiered ERC1155 tokens that
-        update with new rewards, and tokens can be resold on other platforms.
+        Web application to help local theatres manage actor applications for
+        their productions. This was a student project for the capstone course.
       </p>
       <ul className="list-disc" style={{ color: "black", marginLeft: "2em" }}>
         <li style={{ marginBottom: "1rem" }}>
           <p>
-            <a href="https://devpost.com/software/community-funding-w-erc1155">
-              DevPost Page
-            </a>
-          </p>
-        </li>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://github.com/MSBivens/community-funding">
-              Github Repo
-            </a>
+            <a href="https://github.com/MSBivens/TAAM-repo">Github Repo</a>
           </p>
         </li>
       </ul>
-      <h3>Decentralized Job Board MVP for ETHOnline 2022</h3>
-      <p>
-        This web3 job board provides a private, secure space for token-gated job
-        postings, active moderation, and easy resume-based applications
-        submission.
-      </p>
-      <ul className="list-disc" style={{ color: "black", marginLeft: "2em" }}>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://youtu.be/9SqL46uJK2g?si=8E-NIzO5UoF3HuWa">
-              Youtube Demonstration
-            </a>
-          </p>
-        </li>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://vercel.com/msbivens-projects/token-boards-ethonline/48GWmkS38hD6KNaqhNYvRhcy8Rzh">
-              Deployed App
-            </a>
-          </p>
-        </li>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://github.com/MSBivens/token-boards/tree/master">
-              Github Repo
-            </a>
-          </p>
-        </li>
-      </ul>
-      <h3>The Graph</h3>
-      <p>
-        This project builds on the previous projects to create a full dApp with
-        querrying and indexing from{" "}
-        <a href="https://thegraph.com/">The Graph</a>.
-      </p>
-      <ul className="list-disc" style={{ color: "black", marginLeft: "2em" }}>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://vercel.com/msbivens-projects/learn-web3-dao-graph/Gg5PasNiMZPoG72w3cAHx4gGcPLx">
-              Deployed App
-            </a>
-          </p>
-        </li>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://github.com/MSBivens/learn-web3-dao-graph/tree/master">
-              Github Repo
-            </a>
-          </p>
-        </li>
-      </ul>
-      <h3>IPFS Practical</h3>
-      <p>
-        This project creates an NFT collection using Pinata on the Mumbai
-        network and allows a user to mint a part of the collection.
-      </p>
-      <ul className="list-disc" style={{ color: "black", marginLeft: "2em" }}>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://vercel.com/msbivens-projects/learn-web3-dao-ipfs-practical/69pFQWD7eUS7yPLrdkueDEX69H3z">
-              Deployed App
-            </a>
-          </p>
-        </li>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://github.com/MSBivens/learn-web3-dao-ipfs-practical/tree/master">
-              Githuib Repo
-            </a>
-          </p>
-        </li>
-      </ul>
-      <h3>DeFi Exchange</h3>
-      <p>
-        This project creates an DeFi Exchange by building on an ICO app I
-        previously built
-      </p>
-      <ul className="list-disc" style={{ color: "black", marginLeft: "2em" }}>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://vercel.com/msbivens-projects/learn-web3-dao-defi-exchange/8oDQcaSxivfsxVKCbAgbptzAsMxP">
-              Deployed App
-            </a>
-          </p>
-        </li>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://github.com/MSBivens/learn-web3-dao-defi-exchange/tree/master">
-              Github Repo
-            </a>
-          </p>
-        </li>
-      </ul>
-      <h3>DAO and NFT Collection</h3>
-      <p>
-        This project creates an NFT Collection with a website for presales and
-        minting on the Rinkeby network with hardhat and ethers.js.
-      </p>
-      <ul className="list-disc" style={{ color: "black", marginLeft: "2em" }}>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://learn-web3-dao-nft-collection.vercel.app/">
-              Deployed App
-            </a>
-          </p>
-        </li>
-        <li style={{ marginBottom: "1rem" }}>
-          <p>
-            <a href="https://github.com/MSBivens/learn-web3-dao-nft-collection/tree/master">
-              GitHub Repo
-            </a>
-          </p>
-        </li>
-      </ul>
+      {/* END */}
     </div>
   );
 }
